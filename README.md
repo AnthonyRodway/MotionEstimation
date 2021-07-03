9993550c30db67cc358e8a15b26da44a1f815034
AnthonyRodway <anthonyrodway@uvic.ca>
2021-07-02 20:25:37 -0700

    added sad function and grabbing argument line filenames

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # MotionEstimation
-Project Repo for SENG 440 Class on Embedded Systems
+Project Repository for SENG 440 Class on Embedded Systems 
+<br/><br/>
+The user is able to compile all project files with the provided Makefile using the command:
+<pre><code>make
+</code></pre>
+then the user will type the following to run the program:
+<pre><code>./main {input file 1} {input file 2}
+</code></pre>
+If two input files are not handed into the program, then it will return an error
--- main.c
+++ main.c
@@ -3,13 +3,51 @@
 #include <stdbool.h>
 #include <math.h>
 
+/*
+ * function: sadComputation()
+ * parameters: (x, y) position of current block
+ *             (r, s) motion vector(displacement of current block relative to reference block)
+ * return: none
+ * description: according to software solution 1 right now, bound to change...
+ * 
+ */
+void sadComputation(int x, int y, int r, int s){
+    int currentBlock[16][16], referenceBlock[16][16];
+    int difference, sumAbsoluteDifferece = 0;
+    int i, j;
+
+    for(i = 0; i < 16; i++){
+        for(j = 0; j < 16; j++){
+            difference = currentBlock[x - i][y + j] - referenceBlock[(x + r) + i][(y + s) + j];
+            
+            // get the absolute value
+            if(difference < 0){
+                difference -= difference;
+            }
+            sumAbsoluteDifferece += difference;
+        }
+    }
+}
+
+/*
+ * function: main()
+ * parameters: argc - the number of arguments (including ./main)
+ *             *argv[] - array of strings (character pointers) representing the individual arguments provided on the command line
+ * return: none
+ * description: main function
+ * 
+ */
 int main(int argc, char *argv[]){
     // add some argument error checking
     if(argc != 3){
         printf("[Error] Incorrect Number of Arguments.\n");
         return 1;
     }
-    printf("hello");
+
+    char* filenameCurrent = argv[1];
+    char* filenameReference = argv[2];
+
+    printf("%s, %s\n", filenameCurrent, filenameReference);
 
     return 0;
 }
