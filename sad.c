483fc709811f2ecd63173a3d766478d5bd2bbdcf
Shea Faigan <stfaigan@gmail.com>
2021-07-21 15:27:38 -0700

    Implement SAD algorithm in C and Assembly

---
+++ sad.c
@@ -0,0 +1,20 @@
+#define BLOCK_SIZE 8
+
+int calculate_sad(unsigned char **reference_block, unsigned char **current_block, int x, int y, int dx, int dy) {
+    int diff, sad = 0;
+    int i, j;
+
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++){
+            diff = (int) current_block[x + i][y + j] - (int) reference_block[(x + dx) + i][(y + dy) + j];
+            
+            // Get the absolute value
+            if (diff < 0)
+                diff -= diff;
+            else
+                sad += diff;
+        }
+    }
+    
+    return sad; // :(
+}
