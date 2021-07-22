ff134090454463c067339761ffc16b27099db1c0
Shea Faigan <stfaigan@gmail.com>
2021-07-21 19:35:53 -0700

    fix chonk

--- test.c
+++ test.c
@@ -21,10 +21,10 @@ void print_matrix(unsigned char *arr, int rows, int columns) {
 
 void print_chunk(unsigned char arr[8][8], int x, int y) {
     unsigned char chunk[4][4];
+    int i, j = 0;
     memset(chunk, 0, sizeof(chunk[0][0]) * 4 * 4);
-    // memcpy(dest, src, sizeof (mytype) * rows * columns);
-    for (int i = 0; i < 8; i++) {
-        memcpy(chunk[i], arr[i], sizeof(unsigned char) * 4);
+    for (i = 0, j = y; i < 4 && j < 8; i++, j++) {
+        memcpy(chunk[i], arr[j]+x, sizeof(unsigned char) * 4);
     }
     print_matrix(&chunk[0][0], 4, 4);
 }
@@ -32,11 +32,12 @@ void print_chunk(unsigned char arr[8][8], int x, int y) {
 int main() {
     unsigned char arr[8][8];
     int ctr = 0;
+    int i, j = 0;
 
     // memset(arr, 0, sizeof(arr[0][0]) * 8 * 8);
 
-    for (int i = 0; i < 8; i++) {
-        for (int j = 0; j < 8; j++) {
+    for (i = 0; i < 8; i++) {
+        for (j = 0; j < 8; j++) {
             arr[j][i] = ctr++;
         }
     }
@@ -44,7 +45,13 @@ int main() {
     // print_each_element(arr, 8, 8);
     print_matrix(&arr[0][0], 8, 8);
     printf("\n");
-    print_chunk(arr, 0, 0);
+
+    for (i = 0; i < 8; i+=4) {
+        for (j = 0; j < 8; j+=4) {
+            print_chunk(arr, i, j);
+            printf("\n");
+        }
+    }
 
     return 0;
 }
