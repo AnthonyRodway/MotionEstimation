35af267788303f1114b2e87975879d43ac3cc643
Shea Faigan <stfaigan@gmail.com>
2021-08-03 17:14:03 -0700

    cleanup test helpers

--- test.h
+++ test.h
@@ -5,5 +5,3 @@
 void print_each_element(unsigned char *arr, int rows, int columns);
 
 void print_matrix(int height, int width, unsigned char arr[height][width]);
-
-void get_chunk(unsigned char arr[8][8], unsigned char chunk[4][4], int x, int y);
