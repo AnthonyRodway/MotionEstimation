17fb3755b16a40938d48d48cc2a2d3561031b037
Shea Faigan <stfaigan@gmail.com>
2021-08-03 18:14:42 -0700

    refactor test helpers

--- test.c
+++ test.c
@@ -1,18 +1,35 @@
 #include "test.h"
 
-void print_each_element(unsigned char *arr, int rows, int columns) {
-    for (int i = 0; i < columns; i++) {
-        for (int j = 0; j < rows; j++) {
-            printf("arr[%d][%d] = %d\n", j, i, *((arr+i*rows) + j));
-        }
-    }
-}
-
 void print_matrix(int height, int width, unsigned char arr[height][width]) {
-    for (int i = 0; i < height; i++) {
-        for (int j = 0; j < width; j++) {
-            printf("%4d ", arr[i][j]);
+    int x, y;
+
+    for (y = 0; y < height; y++) {
+        for (x = 0; x < width; x++) {
+            printf("%4d ", arr[y][x]);
         }
         printf("\n");
     }
+}
+
+void print_labelled_matrix_to_file(char *filename, int height, int width, unsigned char arr[height][width]) {
+    int x, y;
+    FILE *fp;
+
+    fp = fopen(filename, "w");
+
+    fprintf(fp, "\t  ");
+    for (x = 0; x < width; x++) {
+        fprintf(fp, "%4d ", x);
+    }
+    fprintf(fp, "\n\n");
+
+    for (y = 0; y < height; y++) {
+        fprintf(fp, "%4d  ", y);
+        for (x = 0; x < width; x++) {
+            fprintf(fp, "%4d ", arr[y][x]);
+        }
+        fprintf(fp, "\n");
+    }
+
+    fclose(fp);
 }
--- test.h
+++ test.h
@@ -1,7 +1,3 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-void print_each_element(unsigned char *arr, int rows, int columns);
 
 void print_matrix(int height, int width, unsigned char arr[height][width]);
