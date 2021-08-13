28c5aef2b1a1759b48206063d06b675f1c793a9e
anthonyrodway <anthonyrodway@uvic.ca>
2021-08-13 14:39:20 -0700

    removed unused files

--- test.h
+++
@@ -1,5 +0,0 @@
-#include <stdio.h>
-
-void print_matrix(int height, int width, unsigned char arr[height][width]);
-
-void print_labelled_matrix_to_file(char *filename, int height, int width, unsigned char arr[height][width]);
